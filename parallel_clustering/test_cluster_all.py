dd8d59f481c291092b319a5a9e101de205a4637c
Jessica Shi <jessicashi@gmail.com>
2021-02-20 03:48:22 -0500

    update

--- parallel_clustering/test_cluster_all.py
+++ parallel_clustering/test_cluster_all.py
@@ -44,7 +44,7 @@ def main():
               for nw in num_workers:
                 for i in range(4):
                   out_filename = write_dir + programs_pres[prog_idx] + "_" + pres[file_idx] + "_" + str(r) + "_" + asy + "_" + ref + "_" + moves_pres[move_idx]+"_" + str(nw) + ".out"
-                  ss = ("NUM_THREADS="+nw+" timeout 6h bazel run //clustering:cluster-in-memory_main -- --"
+                  ss = ("NUM_THREADS="+nw+" timeout 6h bazel-1.0.0 run //clustering:cluster-in-memory_main -- --"
                   "input_graph=/home/jeshi/snap/" + filename + " --clusterer_name=" + prog + " "
                   " --clusterer_config='correlation_clusterer_config"
                   " {resolution: " + str(r) + ", subclustering_method: NONE_SUBCLUSTERING, "
