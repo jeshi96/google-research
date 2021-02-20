8cc8003f5237a1d373103438b3dc54434e1af8b5
Jessica Shi <jessicashi@gmail.com>
2021-02-20 04:06:23 -0500

    update

--- parallel_clustering/clustering/cluster-in-memory_main.cc
+++ parallel_clustering/clustering/cluster-in-memory_main.cc
@@ -318,6 +318,7 @@ auto end_read = std::chrono::steady_clock::now();
 PrintTime(begin_read, end_read, "Read");
 
 std::cout << "Num workers: " << pbbs::num_workers() << std::endl;
+std::cout << "Graph: " << input_file << std::endl;
   // Must initialize the list allocator for GBBS, to support parallelism.
   // The list allocator seeds using the number of vertices in the input graph.
   
