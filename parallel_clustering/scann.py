16927c9d8c63fbc56de6c1a4abf3027d0a0ad310
Jessica Shi <jessicashi@gmail.com>
2021-02-22 17:31:22 -0500

    try?

--- parallel_clustering/scann.py
+++ parallel_clustering/scann.py
@@ -9,18 +9,27 @@ import math
 import scann
 from sklearn import datasets as skdatasets
 
-digits = skdatasets.load_digits()
-dataset = digits.data
-target_id = digits.target
-dataset_shape = dataset.shape
-print(dataset_shape)
+#digits = skdatasets.load_digits()
+#dataset = digits.data
+#target_id = digits.target
+#dataset_shape = dataset.shape
+#print(dataset_shape)
+
+with tempfile.TemporaryDirectory() as tmp:
+    response = requests.get("http://ann-benchmarks.com/glove-100-angular.hdf5")
+    loc = os.path.join(tmp, "glove.hdf5")
+    with open(loc, 'wb') as f:
+        f.write(response.content)
+    
+    glove_h5py = h5py.File(loc, "r")
+dataset = glove_h5py['train']
 
 # create scann
 normalized_dataset = dataset / np.linalg.norm(dataset, axis=1)[:, np.newaxis]
 nl = int(math.sqrt(dataset_shape[0]))
-searcher = scann.scann_ops.build(normalized_dataset, 10, "dot_product").tree(
-  num_leaves=2000, num_leaves_to_search=100, training_sample_size=250000).score_ah(
-  2, anisotropic_quantization_threshold=0.2).reorder(100).build()
+searcher = scann.scann_ops_pybind.builder(normalized_dataset, 51, "dot_product").tree(
+    num_leaves=nl, num_leaves_to_search=100, training_sample_size=250000).score_ah(
+    2, anisotropic_quantization_threshold=0.2).reorder(100).build()
 neighbors, distances = searcher.search_batched(dataset, final_num_neighbors=51)
 
 # output neighbor list
@@ -31,7 +40,7 @@ with open("/home/jeshi/snap/digits_50nn", "a+") as out_file:
         out_file.write(str(index) + " " + str(x) + " " + str(distances[index][index_x]) + "\n")
 
 # output target
-with open("/home/jeshi/snap/digits_targetid", "a+") as outfile:
-  for x in target_id:
-    print(str(x) + "\n")
+#with open("/home/jeshi/snap/digits_targetid", "a+") as outfile:
+#  for x in target_id:
+#    print(str(x) + "\n")
 
