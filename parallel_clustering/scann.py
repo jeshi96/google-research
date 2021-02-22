6cd9d457ab21129c91c5ac6c8ed956b57d4e82e5
Jessica Shi <jessicashi@gmail.com>
2021-02-22 17:26:46 -0500

    try?

--- parallel_clustering/scann.py
+++ parallel_clustering/scann.py
@@ -18,8 +18,9 @@ def main():
 
   # create scann
   normalized_dataset = dataset / np.linalg.norm(dataset, axis=1)[:, np.newaxis]
-  nl = math.sqrt(dataset_shape[0])
-  searcher = scann.scann_ops_pybind.builder(normalized_dataset, 51, "dot_product").tree(num_leaves=nl, num_leaves_to_search=100, training_sample_size=250000).score_ah(2, anisotropic_quantization_threshold=0.2).reorder(100).build()
+  nl = int(math.sqrt(dataset_shape[0]))
+  searcher = scann.scann_ops_pybind.builder(normalized_dataset, 51, "dot_product").tree(
+    num_leaves=nl, num_leaves_to_search=100, training_sample_size=250000).score_ah(2, anisotropic_quantization_threshold=0.2).reorder(100).build()
 
   neighbors, distances = searcher.search_batched(dataset, final_num_neighbors=51)
 
