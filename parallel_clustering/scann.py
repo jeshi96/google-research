0c17d1a4a12070349a7b12017584f68e8a66f653
Jessica Shi <jessicashi@gmail.com>
2021-02-22 17:25:03 -0500

    try?

--- parallel_clustering/scann.py
+++ parallel_clustering/scann.py
@@ -18,13 +18,21 @@ def main():
   # create scann
   normalized_dataset = dataset / np.linalg.norm(dataset, axis=1)[:, np.newaxis]
   nl = math.sqrt(dataset_shape[0])
-  searcher = scann.scann_ops_pybind.builder(normalized_dataset, 51, "dot_product").tree(
-    num_leaves=nl, num_leaves_to_search=100, training_sample_size=250000).score_ah(
-    2, anisotropic_quantization_threshold=0.2).reorder(100).build()
+  searcher = scann.scann_ops_pybind.builder(normalized_dataset, 51, "dot_product").tree(num_leaves=nl, num_leaves_to_search=100, training_sample_size=250000).score_ah(2, anisotropic_quantization_threshold=0.2).reorder(100).build()
 
   neighbors, distances = searcher.search_batched(dataset, final_num_neighbors=51)
 
   # output neighbor list
+  with open("/home/jeshi/snap/digits_50nn", "a+") as out_file:
+    for index, row in enumerate(neighbors):
+      for index_x, x in enumerate(row):
+        if x != index:
+          out_file.write(str(index) + " " + str(x) + " " + str(distances[index][index_x]) + "\n")
+
+  # output target
+  with open("/home/jeshi/snap/digits_targetid", "a+") as outfile:
+    for x in target_id:
+      print(str(x) + "\n")
 
 
 if __name__ == "__main__":
