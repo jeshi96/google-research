e9e749e2f96b4af2851ce6388c1bacc4f988c68f
Jessica Shi <jessicashi@gmail.com>
2021-02-22 01:39:09 -0500

    run 9

--- parallel_clustering/runner.py
+++ parallel_clustering/runner.py
@@ -7,13 +7,15 @@ from run_exp_1 import run_1_1, run_1_2, run_1_3
 from run_exp_2 import run_2_corr, run_2_mod
 from run_exp_5 import run_5_corr, run_5_mod
 from run_exp_6 import run_6_corr, run_6_mod
+from run_exp_9 import run_9
 
 def main():
   #run_1_1()
   #run_1_2()
   #run_1_3()
-  run_6_corr()
-  run_6_mod()
+  #run_6_corr()
+  #run_6_mod()
+  run_9()
 
 if __name__ == "__main__":
   main()
