cd8dd22bf3201eb6afbac41ff1079dc18b47b40a
Jessica Shi <jessicashi@gmail.com>
2021-02-23 01:20:26 -0500

    read

--- parallel_clustering/read_cluster_all.py
+++ parallel_clustering/read_cluster_all.py
@@ -26,18 +26,18 @@ def read_and_print(read_filename, num_rounds,avg_obj, avg_time, std_dev_obj, std
       std_dev_time[r_idx] = sqrt(sum([(x - avg_time[r_idx]) ** 2 for x in times]) / num_rounds)
 
 def main():
-  programs = ["ParallelCorrelationClusterer","ParallelModularityClusterer"]
-  programs_pres = ["pc","pm"]
-  files = ["amazon_h","orkut_h"]#"dblp_h", "lj_h","orkut_h","friendster_h"]
-  pres = ["amazon","orkut"]#"dblp","lj","orkut","friendster"]
+  programs = ["ParallelCorrelationClusterer","CorrelationClusterer"]
+  programs_pres = ["pc","c"]
+  files = ["amazon_h","dblp_h", "lj_h","orkut_h"]
+  pres = ["amazon","dblp","lj","orkut"]
   async_sync = ["true"]
-  refines = ["true"] # we already have all, no refine, async
-  moves = ["ALL_MOVE"]
-  moves_pres = ["all"]
-  resolutions = [0.01, 0.85]#[0.00001, 0.0001, 0.001, 0.01, 0.1, 0.25, 0.5, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
+  refines = ["true"]
+  moves = ["NBHR_MOVE"]
+  moves_pres = ["nbhr"]
+  resolutions = [0.00001, 0.0001, 0.001, 0.01, 0.1, 0.25, 0.5, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
   num_workers = [60]#[1, 2, 4, 8, 16, 30, 60]
   read_dir = "/home/jeshi/snap/"
-  write_dir = "/home/jeshi/clustering_out/"
+  write_dir = "/home/jeshi/clustering_out_exp5_new/"
   length = len(files)
   avg_num_clusters = [0]*length
   avg_obj = [0]*length
@@ -46,22 +46,22 @@ def main():
   std_dev_time = [0]*length
   num_rounds = 4
   for prog_idx, prog in enumerate(programs):
-    for r in resolutions:
+    for file_idx, filename in enumerate(files):
       for ref_idx, ref in enumerate(refines):
         for asy_idx, asy in enumerate(async_sync):
           for move_idx, move in enumerate(moves):
             for nw in num_workers:
-              print("Prog: " + prog + ", Resolution: " + str(r) + ", Refine: " + str(ref) + ", Async: " + str(asy) + ", Move: " + str(move))
-              for file_idx, filename in enumerate(files):
+              print("Prog: " + prog + ", File: " + str(pres[file_idx]))
+              for r _idx, r in enumerate(resolutions):
                 read_filename = write_dir + programs_pres[prog_idx] + "_" + pres[file_idx] + "_" + str(r) + "_" + asy + "_" + ref + "_" + moves_pres[move_idx]+"_" + str(nw) + ".out"
                 read_and_print(read_filename, num_rounds, avg_obj, avg_time, std_dev_obj, std_dev_time, file_idx)
               # now we must output
-              for r_idx, x in enumerate(files):
-                print(str(pres[r_idx]), end = "\t")
-                print(avg_time[r_idx], end = "\t")
-                print(std_dev_time[r_idx], end = "\t")
-                print(avg_obj[r_idx], end = "\t")
-                print(std_dev_obj[r_idx], end = "\n")
+              for r_idx, x in enumerate(resolutions):
+                print(str(x) + "\t")
+                print(str(avg_time[r_idx]) + "\t")
+                print(str(std_dev_time[r_idx]) + "\t")
+                print(str(avg_obj[r_idx]) + "\t")
+                print(str(std_dev_obj[r_idx]))
 
 
 if __name__ == "__main__":
