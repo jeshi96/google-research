072f468fefda04a88af3434b6dc07ba19bee0ae8
Jessica Shi <jessicashi@gmail.com>
2021-02-22 23:34:06 -0500

    read

--- parallel_clustering/read_cluster_all.py
+++ parallel_clustering/read_cluster_all.py
@@ -30,10 +30,10 @@ def main():
   programs_pres = ["pc","pm"]
   files = ["amazon_h","orkut_h"]#"dblp_h", "lj_h","orkut_h","friendster_h"]
   pres = ["amazon","orkut"]#"dblp","lj","orkut","friendster"]
-  async_sync = ["true", "false"]
+  async_sync = ["true"]
   refines = ["false"]
-  moves = ["ALL_MOVE"]
-  moves_pres = ["all"]
+  moves = ["NBHR_CLUSTER_MOVE", "NBHR_MOVE"]
+  moves_pres = ["cluster","nbhr"]
   resolutions = [0.01, 0.85]#[0.00001, 0.0001, 0.001, 0.01, 0.1, 0.25, 0.5, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
   num_workers = [60]#[1, 2, 4, 8, 16, 30, 60]
   read_dir = "/home/jeshi/snap/"
@@ -51,17 +51,17 @@ def main():
         for asy_idx, asy in enumerate(async_sync):
           for move_idx, move in enumerate(moves):
             for nw in num_workers:
-              print("Prog: " + prog + ", Resolution: " + str(r) + ", Refine: " + str(ref) + ", Async: " + str(asy) + "Move: " + str(move))
+              print("Prog: " + prog + ", Resolution: " + str(r) + ", Refine: " + str(ref) + ", Async: " + str(asy) + ", Move: " + str(move))
               for file_idx, filename in enumerate(files):
                 read_filename = write_dir + programs_pres[prog_idx] + "_" + pres[file_idx] + "_" + str(r) + "_" + asy + "_" + ref + "_" + moves_pres[move_idx]+"_" + str(nw) + ".out"
                 read_and_print(read_filename, num_rounds, avg_obj, avg_time, std_dev_obj, std_dev_time, file_idx)
               # now we must output
-              for r_idx, r in enumerate(files):
+              for r_idx, x in enumerate(files):
                 print(str(pres[r_idx]), end = "\t")
                 print(avg_time[r_idx], end = "\t")
                 print(std_dev_time[r_idx], end = "\t")
                 print(avg_obj[r_idx], end = "\t")
-                print(std_dev_obj[r_idx], end = "\t")
+                print(std_dev_obj[r_idx], end = "\n")
 
 
 if __name__ == "__main__":
--- parallel_clustering/runner.py
+++ parallel_clustering/runner.py
@@ -3,7 +3,7 @@ import sys
 import signal
 import time
 import subprocess
-from run_exp_1 import run_1_1, run_1_2, run_1_3
+from run_exp_1 import rerun_1_2, rerun_1_3
 from run_exp_2 import run_2_corr, run_2_mod
 from run_exp_5 import run_5_corr, run_5_mod
 from run_exp_6 import run_6_corr, run_6_mod
@@ -11,12 +11,12 @@ from run_exp_9 import run_9
 from run_exp_7 import run_7
 
 def main():
-  #run_1_1()
-  #run_1_2()
+  rerun_1_2()
+  rerun_1_3()
   #run_1_3()
   #run_6_corr()
   #run_6_mod()
-  run_7()
+  #run_7()
 
 if __name__ == "__main__":
   main()
