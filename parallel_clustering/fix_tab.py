6371621049c0765ec5c8fcb878a4a5dfb351195d
Jessica Shi <jessicashi@gmail.com>
2021-02-23 20:55:43 -0500

    exp 8

--- parallel_clustering/fix_tab.py
+++ parallel_clustering/fix_tab.py
@@ -13,7 +13,7 @@ def main():
   output_fp = sys.argv[2]
   with open(output_fp, 'w') as fp:
     for edge in all_edges:
-      print(edge[0] + "\t" + edge[1],flush=True,file=fp)
+      print(edge[0] + "\t" + edge[1] + "\t" + edge[2],flush=True,file=fp)
 
 if __name__ == "__main__":
   main()
