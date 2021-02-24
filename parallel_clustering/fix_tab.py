0bdb3a109f7f9e44d3cd385b2a693c4c587e6a1f
Jessica Shi <jessicashi@gmail.com>
2021-02-23 20:53:30 -0500

    exp 8

---
+++ parallel_clustering/fix_tab.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import time
+
+def main():
+  filename = sys.argv[1]
+  idx = 0
+  all_edges = []
+  with open(filename) as fp:
+    for line in fp:
+      edges = [x.strip() in line.split()]
+      all_edges.append(edges)
+  output_fp = sys.argv[2]
+  with open(output_fp, 'w') as fp:
+    for edge in all_edges
+      print(edge[0] + "\t" + edge[1],flush=True,file=fp)
+
+if __name__ == "__main__":
+  main()
