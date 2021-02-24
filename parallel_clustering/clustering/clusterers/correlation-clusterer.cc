97cf0cb52a5bfa27711c2350c318f639df536065
Jessica Shi <jessicashi@gmail.com>
2021-02-24 01:08:57 -0500

    fix

--- parallel_clustering/clustering/clusterers/correlation-clusterer.cc
+++ parallel_clustering/clustering/clusterers/correlation-clusterer.cc
@@ -91,19 +91,5 @@ absl::Status CorrelationClusterer::RefineClusters(
   return absl::OkStatus();
 }
 
-absl::StatusOr<InMemoryClusterer::Clustering> CorrelationClusterer::Cluster(
-    const ClustererConfig& clusterer_config) const {
-  InMemoryClusterer::Clustering clustering(graph_.Graph()->n);
-
-  // Create all-singletons initial clustering
-  for (std::size_t i = 0; i < graph_.Graph()->n; i++) {
-    clustering[i] = {static_cast<gbbs::uintE>(i)};
-  }
-
-  RETURN_IF_ERROR(RefineClusters(clusterer_config, &clustering));
-
-  return clustering;
-}
-
 }  // namespace in_memory
 }  // namespace research_graph
